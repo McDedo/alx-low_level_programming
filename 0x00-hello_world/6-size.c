dc553340121495eab3e3510f7307e9e18de66817
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-08 07:09:16 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %i bite(s)\n", sizeof(char));
-	printf("Size of an int: %i bite(s)\n", sizeof(int));
-	printf("Size of a long int: %i bite(s)\n", sizeof(long int));
-	printf("Size of a long long int: %i bite(s)\n", sizeof(long long int));
-	printf("Size of a float: %i bite(s)\n", sizeof(float));
+	printf("Size of a char: %lu bite(s)\n", sizeof(char));
+	printf("Size of an int: %lu bite(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bite(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bite(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bite(s)\n", sizeof(float));
 	return (0);
 }
 
