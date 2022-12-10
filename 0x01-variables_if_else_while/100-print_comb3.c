9357f8105a2eea1e78b9bb54590cf37982ece104
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-10 06:27:19 -0800

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combination of two digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	int b;
+
+	for (a = 0; a <= 9; a++)
+		{
+		for (b = a + 1; b <= 9; b++)
+			{putchar(a + '0');
+			putchar(b + '0'); }
+			if (a < 8)
+				{putchar(',');
+				putchar(' '); }}
+	putchar('\n');
+	return (0);
+}
+
