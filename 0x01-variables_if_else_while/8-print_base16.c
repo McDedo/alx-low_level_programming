204db2e1748b9fc08584958ccf283109eafdfbc6
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-10 04:51:50 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 	char la;
 
 	for (num = 0; num < 10; num++)
-		{putchar((num % 10) + '0'); }
+		putchar((num % 10) + '0');
 	for (la = 'a'; num <= 'f'; la++)
-		{putchar(la); }
+		putchar(la);
 	putchar('\n');
 	return (0);
 }
