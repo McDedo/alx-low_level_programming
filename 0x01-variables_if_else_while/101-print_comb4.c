c6fe549a44caacaf66e500412548fab8af24871e
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-12 08:57:41 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible differents combinations of three digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			if (n > i)
+			{
+				for (j = 0; j < 10; j++)
+				{
+					if (j > n)
+					{
+						putchar(i);
+						putchar(n);
+						putchar(j);
+					}
+					if (i == 7 && n == 8 && j == 9)
+					{break; }
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
