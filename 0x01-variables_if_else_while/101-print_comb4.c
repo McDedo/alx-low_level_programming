7bf1596c960f7510fad4a9ba7d31fc51d7ba0822
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-12 09:55:05 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,25 +11,26 @@ int main(void)
 	int n;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (n = 0; n < 10; n++)
+		for (n = 48; n < 58; n++)
 		{
 			if (n > i)
 			{
-				for (j = 0; j < 10; j++)
+				for (j = 48; j < 58; j++)
 				{
 					if (j > n)
 					{
 						putchar(i);
 						putchar(n);
 						putchar(j);
-						if (i == 7 && n == 8 && j == 9)
-						{break; }
+						if (i == 55 && n == 56 && j == 57)
 						{
-							putchar(',');
-							putchar(' ');
+							break;
 						}
+						
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
