3efef72fe3c58128d9d073a911e784079e527595
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-15 08:59:58 -0800

    Task 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit – Write a function that checks for a digit (0 through 9)
+ * _isdigit – Write a function that checks for a digit (0 through 9).
  * @c: input
  * Return: 1 if c is a digit, 0 otherwise
  */
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Write a function that multiplies two integers.
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
