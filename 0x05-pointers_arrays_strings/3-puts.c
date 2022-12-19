e97efbded6af1daee2c4eb0714d8159e40b38b4e
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-19 10:02:52 -0800

    Task 11

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: this is my input string
+ *  @s: this is my input string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
+
