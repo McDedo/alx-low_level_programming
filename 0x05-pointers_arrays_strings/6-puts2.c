250c5c003a179d965108d0229ff140abaf551cc9
Caleb DEDO <dedocaleb1@gmail.com>
2022-12-20 08:30:05 -0800

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,12 +11,14 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
 		{
 			_putchar(str[i]);
 		}
+		else
+			continue;
 	}
 	_putchar('\n');
 }
